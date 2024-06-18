e3ef099eb019ccf4aea9758f6473ec5782ae83b3
SbabalweRoux <sbaviaroux@gmail.com>
2024-06-18 16:31:55 +0200

    Fetched podcasts from api

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from '../src/Components/Navbar'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
   )
 }
---
+++ vite-project/src/Components/Main.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+
+const truncateDescription = (description, wordLimit) => {
+  const words = description.split(" ");
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(" ") + "...";
+  }
+  return description;
+};
+
+const Main = () => {
+  const [podcasts, setPodcasts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPodcasts = async () => {
+      try {
+        const response = await fetch("https://podcast-api.netlify.app");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        const sortedData = data.sort((a, b) => a.title.localeCompare(b.title));
+        setPodcasts(sortedData);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchPodcasts();
+  }, []);
+
+  if (error) {
+    return <div className="text-red-500">Error: {error}</div>;
+  }
+
+  return (
+    <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {podcasts.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        podcasts.map((podcast) => (
+          <div
+            key={podcast.id}
+            className="relative bg-cover bg-center rounded-lg shadow-md h-96"
+            style={{ backgroundImage: `url(${podcast.image})` }}
+          >
+            <div className="absolute inset-0 bg-black opacity-50 rounded-lg"></div>
+            <div className="relative z-10 p-4 flex flex-col justify-end h-full">
+              <h2 className="text-xl font-semibold mb-2 text-white">
+                {podcast.title}
+              </h2>
+
+              <button
+                className="text-gray-300 mb-2"
+                onClick={() => onShowClick(podcast.id)}
+              >
+                <strong>Seasons:</strong> {podcast.seasons}
+              </button>
+              <p className="text-gray-300">
+                <strong>Last Updated:</strong>{" "}
+                {new Date(podcast.updated).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Main;
